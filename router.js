4466c23303e604336e1b199c5e7bc146df0dcbd0
Brianna DelValle <bdelvalle@twilio.com>
2021-07-14 12:17:28 -0600

    Add in some boilerplate to save time

--- router.js
+++ router.js
@@ -1,17 +1,41 @@
 const Router = require('express').Router;
 const cors = require('cors');
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
 const router = new Router();
+
 const votes = [];
 
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 
 
 
 router.post("/welcome", (request, response) => {
+
+})
+
+function generateWelcomeCallerTwiml() {
+
+}
+
+router.post('/pollResponse', (request, response) => {
     
 })
 
+function generatePollResponseTwiML(vote) {
+
+}
+
+
+// router.post("/transcribe", (request, response) => {
+//     console.log('transcribe: ', request.body.TranscriptionText);
+//     response.end();
+// })
+
+// router.post("/recordingStatus", (request, response) => {
+//     saveRecordingUrl(request.body);
+    
+//     response.end();
+// })
 
 
 
@@ -20,6 +44,26 @@ router.post("/welcome", (request, response) => {
 
 
 
+
+
+
+
+
+
+
+// saving votes and recordings to display on frontend application
+function saveRecordingUrl(requestBody) {
+    const voteIndex = votes.findIndex(vote => vote.callSid == requestBody.CallSid);
+    votes[voteIndex].recordingUrl = `${requestBody.RecordingUrl}.mp3`;
+}
+
+function saveVote(vote, callSid) {
+    votes.push({
+        vote,
+        callSid,
+        recordingUrl: null
+    })
+}
 
 
 // this is so my frontend application can GET our responses
